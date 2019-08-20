0d21eb1f36db58488df8949530cabbb90551d79c
martin (darkmtbg2) <martin-baage@hotmail.com>
2019-08-20 08:52:28 +0200

    Added infomation about the Moon

--- DatabaseScripts/InsertValues.sql
+++ DatabaseScripts/InsertValues.sql
@@ -12,8 +12,8 @@ values
 ("Saturn",	60270	,"The planet with large rings",	10.39542,	0.42625,	-185,	11.700,	"Planet"),
 ("Uranus",	25560	,"This is the  Third largest planet Neptune and Uranus is similar to each other",	8.8263,	0.712083333,	-197,	4737,	"Planet"),
 ("Neptune",	24765	,"This Is a blue gas planet that is far from that have",	11.17998,	0.8,	-225,	7000,	"Planet"),
-("Pluto",	1153	,"Pluto was once considered to be a planet",	0.78456,	6.4,	-233,	null,	"Dwarf Planet");
-
+("Pluto",	1153	,"Pluto was once considered to be a planet",	0.78456,	6.4,	-233,	null,	"Dwarf Planet"),
+("Moon", 1737.1,"This the object that lights up the night", 1.62,27.322,-53.15,1426.667,"Satelite");
 
 
 

4ffffd493f32cbf789e06ef86168e09ebd26996e
goket1 <40742575+goket1@users.noreply.github.com>
2019-08-29 10:32:18 +0200

    Created procedures InsertSession and PlanetScanned

--- DatabaseScripts/StoredProcedures.sql
+++ DatabaseScripts/StoredProcedures.sql
@@ -6,3 +6,15 @@ begin
 	select Session,LastscannedTs from Session left outer join LastScanned on LastScanned.SessionID=Session.Session group by Session order by -LastscannedTs desc;
 end //
 delimiter ;
+
+create
+  procedure InsertSession(sessionID varchar(8))
+BEGIN
+    INSERT INTO `SolarSystem`.`Session` (`Session`) VALUES (sessionID);
+END;
+
+create
+    procedure PlanetScanned(sessionID varchar(8), rfid_uid varchar(32))
+begin
+    insert into LastScanned(rfidtag, sessionid) values(rfid_uid, sessionID);
+end;
